38f5628bc9ec6cbc09995d16eb9141b356bdb38f
Tharani2017 <https://github.com/Tharani2017>
2021-02-05 21:30:45 +0530

    created

---
+++ SampPrgm/src/org/cls/sampcls.java
@@ -0,0 +1,15 @@
+package org.cls;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class sampcls {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\jaga\\Desktop\\workspace\\SampPrgm\\chromedriver.exe ");
+	WebDriver d = new ChromeDriver();
+	
+	}
+
+}
---
+++ TestPrgm/src/test/java/prgm/test/testClass.java
@@ -0,0 +1,19 @@
+package prgm.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testClass {
+	
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver d = new ChromeDriver();
+		d.get("https://www.saucedemo.com/");
+	
+	
+	
+	}
+
+}
